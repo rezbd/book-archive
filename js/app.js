eb8d8990d7786e8515d7a0ed08cc53a512aeed9b
rezwan <rezwan6@gmail.com>
2021-09-02 04:27:50 +0600

    show results

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 const searchInput = document.getElementById('search-input');
 const errorContainer = document.getElementById('error-container');
 const bookContainer = document.getElementById('book-container');
+const searchNumber = document.getElementById('search-number');
 
 const searchButton = () => {
     const search = searchInput.value;
@@ -16,16 +17,40 @@ const searchButton = () => {
     const url = `http://openlibrary.org/search.json?q=${search}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => displaySearch(data.docs.splice(0, 8)))
+        .then(data => displaySearch(data))
         .finally(searchInput.value = "")
 }
 
 const displaySearch = (booksArray) => {
+    // number of search results
+    searchNumber.innerHTML = `
+        <p class="text-center">Showing ${booksArray.docs.splice(0, 6).length} of ${booksArray.numFound} results</p>
+    `
+
     // error handling
-    if (booksArray.length === 0) {
-        errorContainer.innerHTML = `<p>NO RESULT FOUND</p>`
+    if (booksArray.numFound === 0) {
+        errorContainer.innerHTML = `<p class="text-center">NO RESULT FOUND</p>`
     } else {
         errorContainer.innerHTML = "";
     }
+
+    // array loop
+
+    booksArray.docs.splice(0, 6).forEach(book => {
+        console.log(book)
+        const div = document.createElement("div");
+        div.classList.add("col");
+        div.innerHTML = `
+            <div class="card h-100">
+                <img src="https://covers.openlibrary.org/b/id/${book.key.cover_i}-L.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+        `
+        bookContainer.appendChild(div);
+    })
 }
 
